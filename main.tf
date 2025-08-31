ea6795eb164992e7ae130a22d8da98ee99d95cb7
Akreddy11 <153039770+Akreddy11@users.noreply.github.com>
2025-08-31 14:10:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,41 @@
-# Copyright 2024 Cisco Systems, Inc. and its affiliates
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# SPDX-License-Identifier: Apache-2.0
+
+
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Single-file friendly: set your values here (or switch back to var.* if you prefer)
+locals {
+  vnet_cfg = {
+    location             = "eastus"
+    vnet_name            = "prod-vnet"
+    ise_resource_group   = "rg-ise-prod"
+    vnet_address         = "10.40.0.0/16"
+    private_subnet_cidrs = ["10.40.1.0/24", "10.40.2.0/24"]
+    public_subnet_cidrs  = ["10.40.10.0/24"]
+    ise_func_subnet_cidr = "10.40.20.0/24"
+    ise_func_subnet      = "ise-func-subnet"
+  }
+}
 
 module "vnet" {
   source               = "./modules/vnet"
-  location             = var.location
-  vnet_name            = var.vnet_name
-  ise_resource_group   = var.ise_resource_group
-  vnet_address         = var.vnet_address
-  private_subnet_cidrs = var.private_subnet_cidrs
-  public_subnet_cidrs  = var.public_subnet_cidrs
-  ise_func_subnet_cidr = var.ise_func_subnet_cidr
-  ise_func_subnet      = var.ise_func_subnet
+  location             = local.vnet_cfg.location
+  vnet_name            = local.vnet_cfg.vnet_name
+  ise_resource_group   = local.vnet_cfg.ise_resource_group
+  vnet_address         = local.vnet_cfg.vnet_address
+  private_subnet_cidrs = local.vnet_cfg.private_subnet_cidrs
+  public_subnet_cidrs  = local.vnet_cfg.public_subnet_cidrs
+  ise_func_subnet_cidr = local.vnet_cfg.ise_func_subnet_cidr
+  ise_func_subnet      = local.vnet_cfg.ise_func_subnet
 }
